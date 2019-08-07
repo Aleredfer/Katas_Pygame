3d06517fb2e47ec6e27513c4231e80641412e961
Aleredfer <aleredfer@hotmail.com>
2019-08-08 00:20:09 +0200

    Pong: bola y racquet

--- main.py
+++ main.py
@@ -1,105 +1,146 @@
 import pygame as pg
 from pygame.locals import *
 import sys
+import random
 
-FPS = 60
 
-class Ball(pg.sprite.Sprite):    #sprite, objetos que se mueven por la pantalla.
-    def __init__(self, x, y):
-        pg.sprite.Sprite.__init__(self)    #inicializa 
-        self.image = pg.Surface((16, 16))   #tamaño
-        self.rect = self.image.get_rect()   #es el rectangulo donde está contenida la imagen.
+
+_FPS = 60
+
+class Raquet(pg.Surface):
+    x=0
+    y=0
+    w= 16
+    h= 96
+    color=(255, 255, 255)
+    velocidad = 5
+    diry =1
+
+    def __init__(self):
+        pg.Surface.__init__(self, (self.w, self.h))
+        self.fill(self.color)
+
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
+
+    def avanza(self):
+        self.y += self.diry * self.velocidad
+
+        if self.y <= 0:
+            self.y = 0
+
+        if self.y >= 504:
+            self.y = 504
         
-        #posicion:
-        self.x = x
-        self.y = y
+        
 
-        #color
-        self.color = (255, 255, 255)  #tupla de 3 colores (RGB)
+class Ball(pg.Surface):    #sprite, objetos que se mueven por la pantalla.
+    x=0
+    y=0
+    w=16
+    h=16
+    velocidad = 5
+    dirx = velocidad
+    diry = velocidad
 
-        #iniciar la parte gráfica
-        self.rect.x = self.x
-        self.rect.y = self.y
-        self.image.fill(self.color)
+    color = (255, 255, 255)  #tupla de 3 colores (RGB)
+    
+    def __init__(self):
+        pg.Surface.__init__(self, (self.w,self.h))
+        self.fill(self.color)
 
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
 
-'''class OtraBall(pg.surface):
-    def __init__(self, x, y):
-        pg.Surface.__init__(self, (16,16))
-        self.fill((255,255,255))
-        self'''
+    def avanza(self):
+        if self.x >= 784:
+            self.dirx = -self.velocidad 
+        if self.y >= 584:
+            self.diry = -self.velocidad 
+        if self.x <= 0:
+            self.dirx = self.velocidad 
+        if self.y <= 0:
+            self.diry = self.velocidad
 
+        self.x += self.dirx
+        self.y += self.diry
+
+    def choqueBall(self, candidata):    #comprueba con el eje de las X en horizontal y con el eje de las Y comprueba en vertical 
+        if (candidata.x >= self.x and candidata.x <= self.x+self.w or \
+            candidata.x+candidata.w >= self.x and candidata.x+candidata.w <= self.x+self.w) and \
+            (candidata.y >= self.x and candidata.y <= self.y+self.h or \
+            candidata.y+candidata.h >= self.y and candidata.y+candidata.h <= self.y+self.h):
+            
+            self.dirx = self.dirx * -1
+            self.x += self.dirx
+               
 
 class Game:    # en la clase Game se controlan los eventos
     clock = pg.time.Clock()      #velocidad de refresco
-    Velocidad = 5
 
     def __init__(self, width, height):
         self.size = (width, height)
         self.display = pg.display    #coge pg.display (objeto: pantalla) y lo mete en un atributo de la class Game para jugar con el.
         self.screen = self.display.set_mode(self.size)   # crea la pantalla   #set_mode() le dice la resolucion a la pantalla(display) o surface
-        self.screen.fill((60,60,60))                    #esto pinta la pantalla "screen" de gris ((60, 60, 60))
+        self.screen.fill((60,60,60))             #esto pinta la pantalla "screen" de gris ((60, 60, 60))
         self.display.set_caption("Mi juego")    #título para la ventana.
 
-        self.ball = pg.Surface((16, 16))
-        self.ball.fill((255,255,255))
-        #self.ball = Ball(392, 292)   #correccion de 8 puntos para que el ''centro esté en el centro''       ## 400 y 300 es el centro de 800x600...
-        #self.ball.get_rect.x =392
-        #self.ball.get_rect.y =292
-        
-        #si quiero otra bola:
-        self.ball1 = pg.Surface((16, 16))
-        self.ball1.fill((255,0,0))       #y la inicializo en start: self.screen.blit(self.ball1, (x+10, y+10))
-        
+        self.ball1 = Ball()
+        self.ball1.setColor((255, 0, 0))
+        self.ball1.x =random.randrange(800)
+        self.ball1.y =random.randrange(600)
+        self.ball1.velocidad = random.randrange(2,9)
+
+        self.player1 = Raquet()
+        self.player1.x= 768
+        self.player1.y= 252
+
     def start(self):
-        x=392
-        y=292
-        dirx = self.Velocidad 
-        diry = self.Velocidad 
-        
-        #bola 2:
-        x1=0
-        y1=0
-
-        dirx1 = self.Velocidad 
-        diry1 = self.Velocidad
-
         while True:  #monta un bucle
-            self.clock.tick(FPS)   #velocidad de fps
+            self.clock.tick(_FPS)   #velocidad de fps
             for event in pg.event.get():  #py.event acumula eventos.    #con get() te da los eventos de esa lista. 
                 if event.type == QUIT:  #es una de las 'locals', un evento del tipo "salir"
                     pg.quit()
                     sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        self.player1.diry = -1
+                        self.player1.velocidad =15
+                        self.player1.avanza()
+
+                    if event.key == K_DOWN:
+                        self.player1.diry = 1
+                        self.player1.velocidad =15
+                        self.player1.avanza()
+
+            #Esto se hace para que no haya que darle muchas pulsaciones y poder MANTENER PRESIONADO el K_UP o el K_DOWN
+            keys_pressed = pg.key.get_pressed()           #devuelve una lista de todas la teclase que han sido pulsadas (de un ciclo a otro?)
+            if keys_pressed[K_UP] == 1:
+                self.player1.diry = -1
+                if self.player1.velocidad <=15:
+                    self.player1.velocidad += 1
+                self.player1.avanza()
+
+        
+            if keys_pressed[K_DOWN]:     #If... : si es True lo hace, no hace falta == 1
+                self.player1.diry = 1
+                if self.player1.velocidad <=15:
+                    self.player1.velocidad += 1  #incrementa velocidad si se mantiene pulsado pero solo hasta 15
+                self.player1.avanza()
+
+
             #modifica la posicion:
-            if x >= 784:
-                dirx = -self.Velocidad 
-            if y >= 584:
-                diry = -self.Velocidad 
-            if x <= 0:
-                dirx = self.Velocidad 
-            if y <= 0:
-                diry = self.Velocidad
-
-            if x1 >= 784:
-                dirx1 = -self.Velocidad 
-            if y1 >= 584:
-                diry1 = -self.Velocidad 
-            if x1 <= 0:
-                dirx1 = self.Velocidad 
-            if y1 <= 0:
-                diry1 = self.Velocidad
-
-
-            x += dirx
-            y += diry
-
-            x1 += dirx1
-            y1 += diry1
+            self.ball1.avanza()
+            self.ball1.choqueBall(self.player1)   # para que cambie de dirección cuando choque con player 1
+          
 
             #Pintar los sprites en el screen:
             self.screen.fill((60, 60, 60))   #esto pinta la pantalla "screen" de gris ((60, 60, 60))
-            self.screen.blit(self.ball, (x, y))  #Metodo blit()  blit(image, (x, y)) posiciona la imagen. Dibuja una imagen dentro de otra: la bola dentro de la pantalla (o sobre)            
-            self.screen.blit(self.ball1, (x1, y1))
+        
+            self.screen.blit(self.ball1, (self.ball1.x , self.ball1.y))
+            self.screen.blit(self.player1, (self.player1.x , self.player1.y))
             self.display.flip()   #repintar   #ampliando, pasa todo lo anterior a la tarjeta gráfica y lo pinta
 
 if __name__ == '__main__':
